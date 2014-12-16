f9a53b16bb46974647d4283ace70cee1e8227dcc
Travers Benoit <btravers@echiquier.educ.insa>
2014-12-16 13:44:45 +0100

    GameTests: Tests

--- src/tests/GameTests.java
+++ src/tests/GameTests.java
@@ -1,25 +1,31 @@
 package tests;
 
+import static org.junit.Assert.*;
 import game.Game;
-import junit.framework.TestCase;
 
-public class GameTests extends TestCase {
+import org.junit.*;
+
+public class GameTests {
 
 	protected Game game;
 	
+	@Before
 	protected void setUp() {
 		game = new Game();
 	}
 	
+	@Test
 	public void testGetCurrentPlay() {
 		assertEquals(game.getCurrentPlayer(), Game.CROSS);
 	}
 	
+	@Test
 	public void testPlay() {
 		game.play(0);
 		assertEquals(game.getCurrentPlayer(), Game.CIRCLE);
 	}
 	
+	@Test
 	public void testUnplay() {
 		game.play(0);
 		game.unplay(0);
