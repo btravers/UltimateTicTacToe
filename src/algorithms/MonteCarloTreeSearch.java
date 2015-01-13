317f108988fc7f99b75b1887b72c780fd792740b
btravers <btravers@insa-rennes.fr>
2015-01-13 15:38:29 +0100

    MonteCarloTreeSearch optimisation

--- src/algorithms/MonteCarloTreeSearch.java
+++ src/algorithms/MonteCarloTreeSearch.java
@@ -9,6 +9,7 @@ public class MonteCarloTreeSearch {
 	
 	class Node {
 		Node parent;
+		List<Node> children;
 		int move;
 		int player;
 		int w;
@@ -16,6 +17,7 @@ public class MonteCarloTreeSearch {
 
 		public Node(Node parent, int m, int player) {
 			this.parent = parent;
+			this.children = new ArrayList<Node>();
 			this.move = m;
 			this.player = player;
 			this.w = 0;
@@ -24,78 +26,42 @@ public class MonteCarloTreeSearch {
 	}
 	
 	private Game game;
+	private Node tree;
 	private int t;
-	private List<Node> successors;
 	
 	public MonteCarloTreeSearch(Game g) {
 		this.game = g;
 		this.t = 0;
-		this.successors = new ArrayList<Node>();
-	}
-	
-	public void expandNode(Node n) {
-		int depth = 0;
-		Node tmp = n;
-		while (tmp.move != -1) {
-			this.game.play(tmp.move);
-			tmp = tmp.parent;
-			depth++;
-		}
 		
-		int p = n.player == Game.CROSS ? Game.CIRCLE : Game.CROSS;
-		
-		for (int s : this.game.getSuccessors()) {
-			this.successors.add(new Node(n, s, p));
-		}
-		
-		for (int i=0; i<depth; i++) {
-			this.game.unplay();
-		}
-	}
-	
-	public void run() {
 		int p = this.game.getCurrentPlayer() == Game.CROSS ? Game.CIRCLE : Game.CROSS;
 		
-		this.expandNode(new Node(null, -1, p));
+		this.tree = new Node(null, -1, p);
+		this.expandNode(this.tree, this.game);
+	}
+	
+	public void expandNode(Node n, Game g) {		
+		int p = n.player == Game.CROSS ? Game.CIRCLE : Game.CROSS;
 		
-		long firstTime = System.currentTimeMillis();
-		
-		Node focus;
-		double bestScore;
-		
-		while (System.currentTimeMillis() - firstTime < 3000) {
-			focus = this.successors.get(0);
-			bestScore = focus.n == 0 ? Integer.MAX_VALUE : ((double)focus.w)/((double)focus.n) + Math.sqrt(2)*Math.sqrt(Math.log(this.t)/focus.n);
-			
-			for (Node n : this.successors) {
-				double tmp = n.n == 0 ? Integer.MAX_VALUE : ((double)n.w)/((double)n.n) + Math.sqrt(2)*Math.sqrt(Math.log(this.t)/n.n);
-				if (tmp > bestScore) {
-					focus = n;
-					bestScore = tmp;
-				}
-			}
-			
-			if (focus.n == 10) {
-				this.expandNode(focus);
-				this.successors.remove(focus);
+		for (int s : g.getSuccessors()) {
+			n.children.add(new Node(n, s, p));
+		}
+	}
+	
+	public void visitNode(Node n, Game g) {
+		if (n.children.isEmpty()) {
+			if (n.n == 10) {
+				this.expandNode(n, g);
 			} else {
-				Game clone = this.game.clone();
-				Node tmp = focus;
-				while (tmp.move != -1) {
-					clone.play(tmp.move);
-					tmp = tmp.parent;
-				}
-				
-				int winner = clone.playOut();
+				int winner = g.playOut();
 				int factor = -1;
-				if (winner == focus.player) {
+				if (winner == n.player) {
 					factor = 1;
 				}
 				if (winner == Game.DRAW) {
 					factor = 0;
 				}
 				
-				tmp = focus;
+				Node tmp = n;
 				while (tmp.move != -1) {
 					tmp.w += factor;
 					factor *= -1;
@@ -104,7 +70,70 @@ public class MonteCarloTreeSearch {
 				}
 				this.t++;
 			}
+		} else {
+			double bestScore = n.children.get(0).n == 0 ? Integer.MAX_VALUE : ((double)n.children.get(0).w)/((double)n.children.get(0).n) + Math.sqrt(2)*Math.sqrt(Math.log(this.t)/n.children.get(0).n);
+			List<Node> successors = new ArrayList<Node>();
+			successors.add(n.children.get(0));
+			
+			for (Node c : n.children) {
+				double tmp = c.n == 0 ? Integer.MAX_VALUE : ((double)c.w)/((double)c.n) + Math.sqrt(2)*Math.sqrt(Math.log(this.t)/c.n);
+				if (tmp == bestScore) {
+					successors.add(c);
+				}
+				if (tmp > bestScore) {
+					successors.clear();
+					successors.add(c);
+					bestScore = tmp;
+				}
+			}
+			
+			Node successor;
+			if (successors.size() == 1) {
+				successor = successors.get(1);
+			} else {
+				int rg = (int)(Math.random()*successors.size());
+				successor = successors.get(rg);
+			}
+			
+			g.play(successor.move);
+			this.visitNode(successor, g);
 		}
+	}
+	
+	public int run() {
+		long firstTime = System.currentTimeMillis();
+		
+		while (System.currentTimeMillis() - firstTime < 3000) {
+			
+			Game clone = this.game.clone();
+			this.visitNode(this.tree, clone);
+		
+		}
+		double bestScore = this.tree.children.get(0).n == 0 ? Integer.MAX_VALUE : ((double)this.tree.children.get(0).w)/((double)this.tree.children.get(0).n) + Math.sqrt(2)*Math.sqrt(Math.log(this.t)/this.tree.children.get(0).n);
+		List<Node> successors = new ArrayList<Node>();
+		successors.add(this.tree.children.get(0));
+		
+		for (Node c : this.tree.children) {
+			double tmp = c.n == 0 ? Integer.MAX_VALUE : ((double)c.w)/((double)c.n) + Math.sqrt(2)*Math.sqrt(Math.log(this.t)/c.n);
+			if (tmp == bestScore) {
+				successors.add(c);
+			}
+			if (tmp > bestScore) {
+				successors.clear();
+				successors.add(c);
+				bestScore = tmp;
+			}
+		}
+		
+		Node successor;
+		if (successors.size() == 1) {
+			successor = successors.get(1);
+		} else {
+			int rg = (int)(Math.random()*successors.size());
+			successor = successors.get(rg);
+		}
+
+		return successor.move;
 	}
 	
 }
