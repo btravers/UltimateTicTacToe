3c990103263289e08aae1c0a44fbf86665f09df0
btravers <benoit.travers@insa-rennes.fr>
2015-01-26 17:38:22 +0100

    Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,7 @@ package main;
 
 import game.Game;
 
+import java.util.List;
 import java.util.Scanner;
 
 import algorithms.AlphaBeta;
@@ -29,6 +30,16 @@ public class Main {
 				MonteCarloTreeSearch mc = new MonteCarloTreeSearch(g);
 				g.play(mc.run(TIMEOUT));
 				System.out.println(g.toString());
+				int move;
+				/*List<Integer> successors = g.getSuccessors();
+				do {
+					System.out.println("Entrez deux entiers entre 1 et 9 (grand carre, petit carre): ");
+					int bigSquare = scan.nextInt()-1;
+					int smallSquare = scan.nextInt()-1;
+					move = bigSquare * 9 + smallSquare;
+				} while (!successors.contains(move));
+				g.play(move);
+				System.out.println(g.toString());*/
 			}
 			tour++;
 		}
